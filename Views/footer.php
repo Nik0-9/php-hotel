417c62105be2376072b66d70b2c57fcf44d3d2b0
Nik0-9 <filannino.nicola9@gmail.com>
2024-05-02 14:32:44 +0200

    INIT scaffolding

---
+++ Views/footer.php
@@ -0,0 +1,8 @@
+</body>
+<header>
+    <div class="container text-center ">
+        <h5>made by me </h5>
+    </div>
+</header>
+
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include __DIR__ . "/Models/hotels.php";
+var_dump($hotels);
+include __DIR__ ."/Views/header.php";
+include __DIR__ ."/Views/footer.php";
